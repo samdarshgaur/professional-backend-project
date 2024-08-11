652cfcaedf6c7ca29db5d4cbb991c84c94debc90
samdarshgaur <samdarshgaur1133@gmail.com>
2024-08-11 17:24:44 +0530

    router and controller configured

--- src/app.js
+++ src/app.js
@@ -9,9 +9,17 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser);
+app.use(cookieParser());
+
+// routes import
+import userRouter from './routes/user.routes.js';
+
+// routes declaration
+// when a user goes to "/users", then the app will give control to userRouter in user.router.js file, which further goes to "/register" and make a post request that hits registerUser method in user.controller.js file.
+// http://localhost:8000/api/v1/users/register
+app.use("/api/v1/users", userRouter);
 
 export { app };
